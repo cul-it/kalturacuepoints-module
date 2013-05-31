9fcc4221982acc5296e1ac005d9a3eb3c7c7736b
James Reidy <jgr25@cornell.edu>
2013-05-31 18:10:49 +0000

    try to include .js file
    git-svn-id: https://svn.library.cornell.edu/kalturacuepoints/trunk@151 27b3a15c-c111-4848-b22a-37a120fd9213

--- module/kalturacuepoints.js
+++ module/kalturacuepoints.js
@@ -41,93 +41,93 @@ function jsCallbackReady(player_id) {
 		myPlayer : null,
 		currentCue : null,
 		firstLoad : false,
-	
+
 		playerPlaying: function() {
 			if( KalturaChaptersSample.firstLoad ) {
 				KalturaChaptersSample.firstLoad = false;
 				var lastIndex = document.URL.lastIndexOf('enterprise');
-				
+
 				var chapterNum = 0;
-				
+
 				if(lastIndex != -1)
 				{
 					xUrl = document.URL.substr(lastIndex + 11); //Remove the 'enterprise/' from the string
 					urlParts = xUrl.split("-");
 					chapterNum = urlParts[0];
 				}
-				
+
 				var element = $('ul li a').get(chapterNum);
 				this.switchActiveCue(element.id);
 				this.jumpToTime(element.getAttribute("data-chapterStartTime"));
-				
+
 				setTimeout( function() {
 					KalturaChaptersSample.myPlayer.sendNotification("doPause");
 				}, 50);
 			}
 		},
-		
+
 		doFirstPlay: function() {
 			KalturaChaptersSample.firstLoad = true;
 			this.myPlayer.sendNotification("doPlay");
 		},
-	
+
 		jumpToTime : function ( timesec ) {
 			if (this.myPlayer != null) {
 				this.myPlayer.sendNotification("doPlay");
 				this.myPlayer.sendNotification("doSeek", timesec/1000);
 			}
 		},
-		
+
 		switchActiveCue : function ( newId ) {
 			if (this.currentCue != null) this.currentCue.className = '';
 			this.currentCue = document.getElementById(newId);
 			this.currentCue.className = 'selected';
 			console.log(newId);
 		},
-  
+
 		cuePointHandler : function( qPoint ) {
 			if(this.firstLoad == false)
 			{
 				var cuePoint = $('#cp'+qPoint.cuePoint.id);
-				
+
 				console.log(cuePoint);
 				this.changePageData(cuePoint);
 			}
-		 },				
-	
+		 },
+
 		//Changes all the data on the page - selecting the chapter, changing the content and the URL
 		changePageData : function(cuePoint){
 			var chapterName = cuePoint.attr("data-chapterName");
-			
+
 			jQuery('#ctitle').text(cuePoint.attr("data-chapterTitle"));
 			jQuery('#cimage').attr("src", cuePoint.attr("data-chapterThumb"));
 			jQuery('#ctags').text("Chapter Tags: " + cuePoint.attr("data-chapterTags"));
 			this.switchActiveCue(cuePoint.attr("id"));
-						
+
 			//Change the URL without refreshing the page
 			window.history.pushState("CuePointClicked", "CuePointClicked", chapterName);
-			
+
 			try
 			{
 				window.history.pushState("CuePointClicked", "CuePointClicked", chapterName);
 			}
 			catch(err)
 			{
-				
+
 			}
 		}
 	}
-	
+
 	// called by the KDP once it is ready to interact with javascript on the page:
 	var jsCallbackReady = function( playerId ) {
 		var player = document.getElementById(playerId);
 		player.addJsListener("playerPlayed", "KalturaChaptersSample.playerPlaying");
 		player.addJsListener("cuePointReached", "KalturaChaptersSample.cuePointHandler");
 		player.addJsListener("mediaReady", "KalturaChaptersSample.doFirstPlay");
-		
+
 		// Cache a reference to kaltura player in a variable within my scope (my object)
 		KalturaChaptersSample.myPlayer = player;
-		
+
 		//myPlayer.addJsListener("adOpportunity", "cuePointHandler"); used for Ad Cue Points
 	};
 
@@ -135,15 +135,15 @@ function jsCallbackReady(player_id) {
     jQuery(document).ready(function() {
 		jQuery('#chapters a').click(function(e) {
 			var chapter = jQuery(e.target);
-	
+
 			//Change the page and skip to the chapter
 			KalturaChaptersSample.changePageData(chapter);
 			KalturaChaptersSample.jumpToTime(chapter.attr("data-chapterStartTime"));
-					
+
 			//Prevent redirect on the page
 			if(e.preventDefault) e.preventDefault();
 			e.returnValue = false; //Fix for IE
-			
+
 			return false;
 		});
 	});
