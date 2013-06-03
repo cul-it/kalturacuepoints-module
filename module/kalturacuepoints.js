5acf7aee7efb6433bdb10fbb36fff9d2142afd7d
James Reidy <jgr25@cornell.edu>
2013-06-03 18:47:49 +0000

    remove one jsCallbackReady and call the other
    git-svn-id: https://svn.library.cornell.edu/kalturacuepoints/trunk@164 27b3a15c-c111-4848-b22a-37a120fd9213

--- module/kalturacuepoints.js
+++ module/kalturacuepoints.js
@@ -118,7 +118,7 @@ function jsCallbackReady(player_id) {
 	}
 
 	// called by the KDP once it is ready to interact with javascript on the page:
-	var jsCallbackReady = function( playerId ) {
+/*	var jsCallbackReady = function( playerId ) {
 		var player = document.getElementById(playerId);
 		player.addJsListener("playerPlayed", "KalturaChaptersSample.playerPlaying");
 		player.addJsListener("cuePointReached", "KalturaChaptersSample.cuePointHandler");
@@ -146,13 +146,14 @@ function jsCallbackReady(player_id) {
 			return false;
 		});
 	});
-
+*/
 (function ($) {
   $(document).ready(function(){
 		//jsCallbackReady('#kaltura_player_9511');
 		var embed = document.getElementsByClassName("kaltura-embed")[0];
 		var obj = embed.getElementsByTagName("object")[0];
 		var playerId = obj.getAttribute('id');
+		jsCallbackReady(playerId);
 		var player = document.getElementById(playerId);
 		if (player != null) {
 			player.addJsListener("playerPlayed", "KalturaChaptersSample.playerPlaying");
