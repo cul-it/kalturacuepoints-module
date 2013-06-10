b705f50b964249d08fa008a041dc6271e82d435b
James Reidy <jgr25@cornell.edu>
2013-06-10 20:14:57 +0000

    remove unused code
    git-svn-id: https://svn.library.cornell.edu/kalturacuepoints/trunk@204 27b3a15c-c111-4848-b22a-37a120fd9213

--- module/kalturacuepoints.js
+++ module/kalturacuepoints.js
@@ -53,45 +53,6 @@ var KalturaChaptersSample = {
 			}
 		},
 
-		switchActiveCue : function ( newId ) {
-			if (this.currentCue != null) this.currentCue.className = '';
-			this.currentCue = document.getElementById(newId);
-			this.currentCue.className = 'selected';
-			console.log(newId);
-		},
-
-		cuePointHandler : function( qPoint ) {
-			if(this.firstLoad == false)
-			{
-				var cuePoint = $('#cp'+qPoint.cuePoint.id);
-
-				console.log(cuePoint);
-				this.changePageData(cuePoint);
-			}
-		 },
-
-		//Changes all the data on the page - selecting the chapter, changing the content and the URL
-		changePageData : function(cuePoint){
-			var chapterName = cuePoint.attr("data-chapterName");
-
-			jQuery('#ctitle').text(cuePoint.attr("data-chapterTitle"));
-			jQuery('#cimage').attr("src", cuePoint.attr("data-chapterThumb"));
-			jQuery('#ctags').text("Chapter Tags: " + cuePoint.attr("data-chapterTags"));
-			this.switchActiveCue(cuePoint.attr("id"));
-
-			//Change the URL without refreshing the page
-			window.history.pushState("CuePointClicked", "CuePointClicked", chapterName);
-
-			try
-			{
-				window.history.pushState("CuePointClicked", "CuePointClicked", chapterName);
-			}
-			catch(err)
-			{
-
-			}
-		},
-
 	updatePlayheadHandler : function(data, id) {
     // data = the player's progress time in seconds
     // id = the ID of the player that fired the notification;
@@ -111,59 +72,9 @@ var KalturaChaptersSample = {
 		var player = document.getElementById(playerId2);
 		//alert(playerId + ' from dynamic callback - real id: ' + playerId2);
 		player.addJsListener("playerPlayed", "KalturaChaptersSample.playerPlaying");
-		player.addJsListener("cuePointReached", "KalturaChaptersSample.cuePointHandler");
 		player.addJsListener("mediaReady", "KalturaChaptersSample.doFirstPlay");
 		player.addJsListener("playerUpdatePlayhead", "KalturaChaptersSample.updatePlayheadHandler");
 
 		// Cache a reference to kaltura player in a variable within my scope (my object)
 		KalturaChaptersSample.myPlayer = player;
-
-		//player.sendNotification("doPlay");
-		player.sendNotification('doSeek', parseFloat('5.992'));
-		player.jumpToTime('5992');
-
-		//myPlayer.addJsListener("adOpportunity", "cuePointHandler"); used for Ad Cue Points
 	};
-
-    jQuery.noConflict();
-    jQuery(document).ready(function() {
-		jQuery('#chapters a').click(function(e) {
-			var chapter = jQuery(e.target);
-
-			//Change the page and skip to the chapter
-			KalturaChaptersSample.changePageData(chapter);
-			KalturaChaptersSample.jumpToTime(chapter.attr("data-chapterStartTime"));
-
-			//Prevent redirect on the page
-			if(e.preventDefault) e.preventDefault();
-			e.returnValue = false; //Fix for IE
-
-			return false;
-		});
-	});
-
-(function ($) {
-  $(document).ready(function(){
-		//jsCallbackReady('#kaltura_player_9511');
-		// var embed = document.getElementsByClassName("kaltura-embed")[0];
-		// var obj = embed.getElementsByTagName("object")[0];
-		// var playerId = obj.getAttribute('id');
-		// Seek to: <input type="text" size="4" id="seekto" value="25"/><button onclick="$('#kdp3').get(0).sendNotification('doSeek', parseFloat($('#seekto').val()));return false;">Seek</button>
-		// obj.sendNotificaton('doSeek', parseFloat(13));
-		//jsCallbackReady(playerId);
-/*		var player = document.getElementById(playerId);
-		if (player != null) {
-			player.sendNotification("doPlay");
-			player.addJsListener("playerPlayed", "KalturaChaptersSample.playerPlaying");
-			player.addJsListener("cuePointReached", "KalturaChaptersSample.cuePointHandler");
-			player.addJsListener("mediaReady", "KalturaChaptersSample.doFirstPlay");
-
-			// Cache a reference to kaltura player in a variable within my scope (my object)
-			KalturaChaptersSample.myPlayer = player;
-		}
-
-	window.KalturaChaptersSample.jumpToTime(13);
-*/  });
-})(jQuery);
-
-
