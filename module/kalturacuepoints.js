8cf808825d7c02f2f755274c93b74e76c8b401f7
James Reidy <jgr25@cornell.edu>
2014-06-11 18:22:12 +0000

    pause
    git-svn-id: https://svn.library.cornell.edu/kalturacuepoints/trunk@264 27b3a15c-c111-4848-b22a-37a120fd9213

--- module/kalturacuepoints.js
+++ module/kalturacuepoints.js
@@ -40,6 +40,7 @@ var KalturaChaptersSample = {
 			if( KalturaChaptersSample.firstLoad ) {
 				KalturaChaptersSample.firstLoad = false;
 				KalturaChaptersSample.myPlayer.removeJsListener("playerPlayed", "KalturaChaptersSample.playerPlaying");
+				KalturaChaptersSample.clickOnPause();
 			}
 		},
 
@@ -48,7 +49,6 @@ var KalturaChaptersSample = {
 			KalturaChaptersSample.findSegmentStartEnd();
 			KalturaChaptersSample.jumpToTime(KalturaChaptersSample.segmentStart);
 			KalturaChaptersSample.myPlayer.removeJsListener("mediaReady", "KalturaChaptersSample.doFirstPlay");
-			KalturaChaptersSample.clickOnPause();
 		},
 
 		clickOnPause: function() {
