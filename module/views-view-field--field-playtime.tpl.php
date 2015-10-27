df3c2746e3c96fc1e0846c59c8c2583f2cb46933
James Reidy <jgr25@cornell.edu>
2015-10-27 20:12:48 +0000

    more to show
    git-svn-id: https://svn.library.cornell.edu/kalturacuepoints/trunk@355 27b3a15c-c111-4848-b22a-37a120fd9213

--- module/views-view-field--field-playtime.tpl.php
+++ module/views-view-field--field-playtime.tpl.php
@@ -24,9 +24,9 @@
 ?>
 <?php print $output;
   /* Your code goes here. */
-  // $playtime = $row{$field->field_playtime};
-  // $out = array('playtime' => $playtime);
-  // dpm($out);
+  $data = $row{$field->field_playtime};
+  $out = array('playtime' => $data);
+  dpm($out);
   dpm($output);
   if($output) {
     dpm($row);
